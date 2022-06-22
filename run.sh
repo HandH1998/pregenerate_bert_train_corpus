f3f35af5673f439f0a011367cc430f7defce3c47
zhangying.1998 <zhangying.1998@bytedance.com>
2022-06-22 23:10:09 +0800

    first add

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# rootPath=$(dirname $(readlink -f $0))
+# echo $rootPath
+while read requirement; do conda install --yes $requirement || pip install $requirement; done < requirements.txt
+pythonExe=/root/anaconda3/envs/pregenerate/bin/python3
+hdfs dfs -get hdfs://haruna/home/byte_arnold_hl_mlnlc/user/zhangying.1998/bert-large-uncased
+hdfs dfs -get hdfs://haruna/home/byte_arnold_hl_mlnlc/user/zhangying.1998/sharded_training_shards_256_test_shards_256_fraction_0.0/books_wiki_en_corpus
+$pythonExe -u pregenerate_training_data.py \
+        --train_corpus books_wiki_en_corpus/ \
+        --output_dir  pretraining_bert_data/ \
+        --bert_model bert-large-uncased/ \
+        --do_lower_case \
+        --num_workers 16 \
+        --epochs_to_generate 3 \
+        --max_seq_len 128 \
+        --short_seq_prob 0.1 \
+        --masked_lm_prob 0.0 \
+        --max_predictions_per_seq 20 
+hdfs dfs -put pretraining_bert_data/ hdfs://haruna/home/byte_arnold_hl_mlnlc/user/zhangying.1998/
