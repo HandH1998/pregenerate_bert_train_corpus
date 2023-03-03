9fb9f7246b6cda193116e7358e42620f15d7a420
zhangying.1998 <zhangying.1998@bytedance.com>
2023-03-03 16:07:23 +0800

    fix bugs

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 # rootPath=$(dirname $(readlink -f $0))
 # echo $rootPath
 # while read requirement; do conda install --yes $requirement || pip install $requirement; done < requirements.txt
-hdfs dfs -get hdfs://haruna/home/byte_arnold_hl_mlnlc/user/zhangying.1998/bert-large-uncased
-hdfs dfs -get hdfs://haruna/home/byte_arnold_hl_mlnlc/user/zhangying.1998/sharded_training_shards_256_test_shards_256_fraction_0.0/books_wiki_en_corpus
+get bert-large-uncased
+get sharded_training_shards_256_test_shards_256_fraction_0.0/books_wiki_en_corpus
 python3 -u pregenerate_training_data.py \
         --train_corpus books_wiki_en_corpus/ \
         --output_dir  pretraining_bert_data/ \
@@ -16,4 +16,3 @@ python3 -u pregenerate_training_data.py \
         --short_seq_prob 0.1 \
         --masked_lm_prob 0.0 \
         --max_predictions_per_seq 20 
-hdfs dfs -put pretraining_bert_data/ hdfs://haruna/home/byte_arnold_hl_mlnlc/user/zhangying.1998/
